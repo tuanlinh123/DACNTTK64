50b4356cc5c18265705ef5095de40d1428bfaebf
anhpd17 <anhpd17@gmail.com>
2024-11-03 11:31:45 +0700

    xây dwngjt hêm màn giáo viên và người dùng

---
+++ pages/users/gridConfigUsers.js
@@ -0,0 +1,41 @@
+export const gridConfigUsers = [
+    {
+        headerName: "STT",
+        width: 100,
+    },
+    {
+        field: "userCode",
+        headerName: "Mã người dùng",
+        width: 350,
+    },
+    {
+        field: "userName",
+        headerName: "Họ tên",
+        width: 300,
+    },
+    {
+        field: "role",
+        headerName: "Vai trò",
+        width: 300,
+    },
+    {
+        field: "dateOfBirth",
+        headerName: "Ngày sinh",
+        width: 250,
+    },
+    {
+        field: "gender",
+        headerName: "Giới tính",
+        width: 300,
+    },
+    {
+        field: "email",
+        headerName: "Email",
+        width: 200,
+    },
+    {
+        field: "phoneNumber",
+        headerName: "Số điện thoại",
+        width: 200,
+    },
+];
--- pages/users/users.js
+++ pages/users/users.js
@@ -1,15 +1,15 @@
-import { gridConfigClasses } from "./gridConfigClasses.js";
+import { gridConfigUsers } from "./gridConfigUsers.js";
 import { getData } from "../../firebase/firebaseMethod.js";
 import { getCodeRenderHeader, getCodeRenderGrid } from "../../core/grid.js";
 
-const renderGridClasses = async () => {
-    let dataClasses = await getData("classes");
+const renderGridUsers = async () => {
+    let dataClasses = await getData("teachers");
     document.getElementById("header-classes").innerHTML =
-        getCodeRenderHeader(gridConfigClasses);
+        getCodeRenderHeader(gridConfigUsers);
     document.getElementById("grid-classes").innerHTML = getCodeRenderGrid(
         dataClasses,
-        gridConfigClasses
+        gridConfigUsers
     );
 };
 
-renderGridClasses();
+renderGridUsers();
