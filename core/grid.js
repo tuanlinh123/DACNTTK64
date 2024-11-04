2dd48430c31e59738bf32592e5f0a46fcf7e4bb1
anhpd17 <anhpd17@gmail.com>
2024-11-04 18:48:09 +0700

    Sửa lại code grid

--- core/grid.js
+++ core/grid.js
@@ -26,17 +26,10 @@ export const getCodeRenderGrid = (dataClasses, gridConfig) => {
                         }
                         // Format cột chức năng
                         else if (item1.field === "function") {
-                            return `<td style="border: 1px solid #e0e0e0; text-align: ${
-                                item1.align ?? "left"
-                            }">
-                            <button type="button" class="btn btn-secondary">Chi tiết</button>
-                        </td>`;
+                            return item1.customDisplay();
                         }
-                        // Format cột giới tính
-                        else if (item1.field === "gender") {
-                            return `<td style="border: 1px solid #e0e0e0; text-align: center">${
-                                item[item1.field] == "male" ? "Nam" : "Nữ"
-                            }</td>`;
+                        if (item1.customDisplay) {
+                            return item1.customDisplay(item[item1.field]);
                         }
                         // Mặc định các cột
                         return `<td style="border: 1px solid #e0e0e0; text-align: ${
--- pages/classes/gridConfigClasses.js
+++ pages/classes/gridConfigClasses.js
@@ -17,6 +17,14 @@ export const gridConfigClasses = [
         field: "status",
         headerName: "Trạng thái",
         width: 250,
+        customDisplay: (value) => {
+            let mappingStatus = {
+                Pending: "Đang chờ",
+                Completed: "Đã kết thúc",
+                Running: "Đang hoạt động",
+            };
+            return `<td style="border: 1px solid #e0e0e0; text-align: left">${mappingStatus[value]}</td>`;
+        },
     },
     {
         field: "teacherName",
@@ -34,5 +42,10 @@ export const gridConfigClasses = [
         headerName: "Thao tác",
         width: 200,
         align: "center",
+        customDisplay: () => {
+            return `<td style="border: 1px solid #e0e0e0; text-align: center">
+                <button type="button" class="btn btn-secondary">Chi tiết</button>
+            </td>`;
+        },
     },
 ];
